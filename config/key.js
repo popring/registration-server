268259aa1e317b0c0e8b503aecea52767ea3178c
HarryHao <koler778@gmail.com>
2020-06-15 09:15:22 +0800

    修复无法正常读取的私钥

--- config/key.js
+++ config/key.js
@@ -3,7 +3,12 @@ const path = require("path");
 
 const filePath = path.resolve(__dirname, "../PrivateKey.key");
 
-const PRIVATEKEY =
-  fs.readFileSync(filePath).toString() || "zsb_https://github.com/popring";
+let PRIVATEKEY;
+
+try {
+  PRIVATEKEY = fs.readFileSync(filePath, { encoding: "utf-8" });
+} catch (error) {
+  PRIVATEKEY = "zsb_https://github.com/popring";
+}
 
 module.exports = PRIVATEKEY;
