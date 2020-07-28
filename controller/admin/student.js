70c40ea9689c986a221a1713de940a79326523b1
Harry Hao <koler778@gmail.com>
2020-07-28 11:31:46 +0800

    完成学生管理接口，未做分页

--- controller/admin/student.js
+++ controller/admin/student.js
@@ -2,33 +2,100 @@ const models = require("../../models");
 const tips = require("../../config/Tips");
 
 /**
- * 查询学生列表
+ * 查询指定学生信息
  * @param sid 学生id
  * @returns {Promise<{code: number, data: *, message: string}>}
  */
-async function findStudent (sid) {
-  let data;
-  if (!sid) {
-    // 查询所有学生
-    // TODO 分页功能
-    const student = await models.Student.findAll({
-      attributes: ["Sid", "Sname", "Sphone", "Sschool"],
-      include: ["Major"],
-    });
-    data = student && student.map(item => item.dataValues);
-  } else {
-    // 查询指定学生信息
-    const student = await models.Student.findOne({
-      include: ["Major"],
-      where: {
-        sid,
-      },
-    });
-    data = student && student.dataValues;
-  }
+async function findOneStudent(sid) {
+  if (!sid)
+    return { ...tips.GET_INFO_FAILED, message: "sid值错误" };
+  const student = await models.Student.findOne({
+    include: ["Major"],
+    where: {
+      sid,
+    },
+  });
+  let data = student && student.dataValues;
   return { ...tips.GET_INFO_SUCCESS, data };
 }
 
+/**
+ * 查询学生列表
+ * @param opt 可选查询参数 包括limit,offset
+ * @returns {Promise<{code: number, data: *, message: string}>}
+ */
+async function findAllStudent(opt = {}) {
+  opt.limit = Number.parseInt(opt.limit) || 10;
+  opt.offset = Number.parseInt(opt.offset) || 0;
+  const student = await models.Student.findAndCountAll({
+    attributes: ["Sid", "Sname", "Sphone", "Sschool"],
+    include: ["Major"],
+    // 查询数量
+    limit: opt.limit,
+    // 偏移
+    offset: opt.offset,
+    // sid 倒序搜索
+    order: [
+      ["sid", "DESC"],
+    ],
+  });
+  let data = student && student.rows.map(item => item.dataValues);
+  return {
+    ...tips.GET_INFO_SUCCESS,
+    tableData: {
+      limit: opt.limit,
+      offset: opt.offset,
+      count: student.count,
+      data: data,
+    },
+  };
+}
+
+/**
+ * 修改学生信息
+ * @param userInfo 学生信息
+ * @returns {Promise<{code: number, message: string}>}
+ */
+async function updateStudent(userInfo) {
+  if (!userInfo.Sid)
+    return { ...tips.OPERATE_FAILED, message: "sid值错误" };
+
+  const stu = await models.Student.findOne({
+    where: {
+      sid: userInfo.Sid,
+    },
+  });
+  for (const userInfoKey in userInfo) {
+    stu.set(userInfoKey, userInfo[userInfoKey]);
+  }
+  const res = await stu.save();
+  if (res instanceof models.Sequelize.ValidationError) {
+    return tips.OPERATE_FAILED;
+  }
+  return tips.OPERATE_SUCCESS;
+}
+
+/**
+ * 删除学生
+ * @param sid 学生id
+ * @returns {Promise<{code: number, message: string}>}
+ */
+async function deleteStudent(sid) {
+  const res = await models.Student.destroy({
+    where: {
+      sid: sid,
+    },
+    force: true,
+  });
+  if (res === 0) {
+    return { ...tips.OPERATE_FAILED, message: "删除失败，sid无效" };
+  }
+  return tips.OPERATE_SUCCESS;
+}
+
 module.exports = {
-  findStudent,
+  findOneStudent,
+  findAllStudent,
+  updateStudent,
+  deleteStudent,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -6,20 +6,32 @@ const router = express.Router();
 
 router.use(adminAuth);
 
-router.get("/process", async function (req, res) {
+router.get("/process", async function(req, res) {
   const data = await ControllerProcess.findOne(202002);
-  res.json({data: data});
+  res.json({ data: data });
 });
 
 // 查询学生
 router.get("/student", async function(req, res) {
-  const data = await ControllerStudent.findStudent();
+  const data = await ControllerStudent.findAllStudent(req.query);
   res.send(data);
 });
 
-// 查询学生
+// 查询学生信息
 router.get("/student/:sid", async function(req, res) {
-  const data = await ControllerStudent.findStudent(req.params.sid);
+  const data = await ControllerStudent.findOneStudent(req.params.sid);
+  res.send(data);
+});
+
+// 修改学生信息
+router.put("/student", async function(req, res) {
+  const data = await ControllerStudent.updateStudent(req.body);
+  res.send(data);
+});
+
+// 删除学生
+router.delete("/student/:sid", async function(req, res) {
+  const data = await ControllerStudent.deleteStudent(req.params.sid);
   res.send(data);
 });
 
